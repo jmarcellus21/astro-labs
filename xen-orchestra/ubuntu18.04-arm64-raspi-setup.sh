64c8dec92e9c166d8dda760875af6dd3ecb7eedc
jmarcellus21 <jerry.marcellus@gmail.com>
2019-06-24 21:24:25 -0400

    initial commit

---
+++ xen-orchestra/ubuntu18.04-arm64-raspi-setup.sh
@@ -0,0 +1,12 @@
+# Disable auto-updating service
+sudo systemctl disable apt-daily.timer
+sudo systemctl disable apt-daily-upgrade.timer
+
+# Set time-zone
+sudo timedatectl set-timezone America/New_York
+
+# Update all packages
+sudo apt-get update -y && sudo apt-get upgrade -y
+
+# Add swap file
+sudo apt-get install dphys-swapfile
