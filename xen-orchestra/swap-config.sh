621848a11d696d53238bdb4bfd3872b530f79f0a
jmarcellus21 <jerry.marcellus@gmail.com>
2019-06-25 18:06:31 -0400

    restart dphys-swapfile service

--- xen-orchestra/swap-config.sh
+++ xen-orchestra/swap-config.sh
@@ -8,3 +8,7 @@ cat raspi3b-swap-config.txt > /etc/dphys-swapfile
 
 echo "**Cleaning up"
 rm raspi3b-swap-config.txt
+
+echo "**Restarting swapfile service"
+sudo /etc/init.d/dphys-swapfile stop
+sudo /etc/init.d/dphys-swapfile start
