5ee7f69f35f6199ec0ff04a3a3be228e24a432cc
jmarcellus21 <jerry.marcellus@gmail.com>
2019-06-28 18:59:01 -0400

    install unifi dependencies

--- unifi/unifi-controller-install.sh
+++ unifi/unifi-controller-install.sh
@@ -8,6 +8,9 @@ echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb
 # Reload package database
 sudo apt-get update
 
+# Install Unifi Controller dependencies
+sudo apt-get install -y mongodb-org openjdk-8-jre-headless jsvc
+
 # Install Unifi controller
 cd /tmp/
 wget https://dl.ubnt.com/unifi/5.10.25/unifi_sysvinit_all.deb
