f3e278ff9ee1d612a7db685c228d30f27ffcd411
jmarcellus21 <jerry.marcellus@gmail.com>
2019-08-01 20:29:26 -0400

    first-use configuration

--- raspi-ubuntu18-setup/setup.sh
+++ raspi-ubuntu18-setup/setup.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+# Disable auto-updating service
+sudo systemctl disable apt-daily.timer
+sudo systemctl disable apt-daily-upgrade.timer
+
+# Set time-zone
+sudo timedatectl set-timezone America/New_York
+
+# Update all packages
+sudo apt-get update -y && sudo apt-get upgrade -y
+
+# Install Git Subversion
+sudo apt-get install subversion
+
 # get mac address of eth0 interface
 ifconfig eth0 | grep -o "..:..:..:..:..:.."
