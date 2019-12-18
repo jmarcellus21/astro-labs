6200bcb182602e92a62ecaef5602e0a53ab11f4b
jmarcellus21 <jerry.marcellus@gmail.com>
2019-12-17 20:41:37 -0500

    Update k3s-install.sh

--- k3s/k3s-install.sh
+++ k3s/k3s-install.sh
@@ -5,9 +5,15 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 # check if line is there, insert if needed
 sudo sed -i '$ s/$/ cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory/' /boot/firmware/cmdline.txt
 
+# reboot after editing boot parameters
 sudo reboot now
-sudo apt-get install docker.io
+
+# install docker and k3s
+sudo apt-get install -y docker.io
 curl -sfL https://get.k3s.io | sh -
 
 # verify install finished properly
 sudo k3s kubectl get nodes
+
+# add alias for kubectl command
+alias kubectl='k3s kubectl'
