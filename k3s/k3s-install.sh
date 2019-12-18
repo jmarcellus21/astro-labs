f8341cf58f5fd5037aaa1a488b16d513a9c7e44f
jmarcellus21 <jerry.marcellus@gmail.com>
2019-12-17 20:30:59 -0500

    reboot after editing boot parameters

--- k3s/k3s-install.sh
+++ k3s/k3s-install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 sudo apt-get update -y && sudo apt-get upgrade -y
+
+# check if line is there, insert if needed
 sudo sed -i '$ s/$/ cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory/' /boot/firmware/cmdline.txt
+
+sudo reboot now
 sudo apt-get install docker.io
 curl -sfL https://get.k3s.io | sh -
 
