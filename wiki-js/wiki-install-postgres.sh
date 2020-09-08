f661ffeccf95e836078a643eecf819489bfc6359
jmarcellus21 <jerry.marcellus@gmail.com>
2020-09-08 09:09:07 -0400

    update to version 2.5.126

--- wiki-js/wiki-install-postgres.sh
+++ wiki-js/wiki-install-postgres.sh
@@ -17,7 +17,7 @@ sudo apt update
 sudo apt install -y postgresql nodejs
 
 # download and install WikiJS
-wget -P /tmp https://github.com/Requarks/wiki/releases/download/2.4.107/wiki-js.tar.gz
+wget -P /tmp https://github.com/Requarks/wiki/releases/download/2.5.126/wiki-js.tar.gz
 sudo mkdir -p /opt/wiki/db
 sudo tar xzf /tmp/wiki-js.tar.gz -C /opt/wiki
 cd /opt/wiki
