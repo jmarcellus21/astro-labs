a12136eda5804b17e34bef239ff027d60d4a65ff
jmarcellus21 <jerry.marcellus@gmail.com>
2020-10-20 16:44:22 -0400

    create installation directory

--- wiki-js/wiki-install-postgres.sh
+++ wiki-js/wiki-install-postgres.sh
@@ -30,6 +30,7 @@ sudo apt install -y postgresql nodejs
 
 # download and install WikiJS
 wget https://github.com/Requarks/wiki/releases/download/$LATEST_VERSION/wiki-js.tar.gz
+mkdir $INSTALL_DIR
 sudo tar xzf wiki-js.tar.gz -C $INSTALL_DIR
 cd $INSTALL_DIR
 cp config.sample.yml config.yml
