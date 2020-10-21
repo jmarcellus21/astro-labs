13f92569de2a79a084f1c129653ccd357aa3e026
jmarcellus21 <jerry.marcellus@gmail.com>
2020-10-21 09:27:30 -0400

    add echo statements

--- wiki-js/wikijs-upgrade.sh
+++ wiki-js/wikijs-upgrade.sh
@@ -12,27 +12,33 @@ SERVICE_NAME="wiki"
 INSTALL_DIR="/opt/wiki"
 
 LATEST_VERSION=$(get_latest_release $REPO)
-echo "HEllo v$LATEST_VERSION"
+echo "Latest Release: v$LATEST_VERSION"
 
 # stop wiki service
 sudo systemctl stop $SERVICE_NAME
 
 #backup config.yml file
+echo "## Backing up config.yml"
 sudo cp $INSTALL_DIR/config.yml ~/config.yml.bak
 
 # delete application folder
+echo "## Removing existing application folder"
 sudo rm -rf $INSTALL_DIR/*
 
 # dowload latest version of wiki.js
+echo "## Downloading latest release"
 wget --quiet https://github.com/Requarks/wiki/releases/download/$LATEST_VERSION/wiki-js.tar.gz -O wiki-js.tar.gz
 
 # extract the package to installation directory
+echo "## Extracting package to installation directory"
 sudo tar xzf wiki-js.tar.gz -C $INSTALL_DIR
 
 # restore config.yml file
+echo "## Restoring the config.yml file"
 cp ~/config.yml.bak $INSTALL_DIR/config.yml
 
 # give wikijs service account ownership of files
+echo "## Give wikijs service account ownership of installation directory"
 sudo chown -R wikijs:wikijs $INSTALL_DIR/*
 
 # start wiki
